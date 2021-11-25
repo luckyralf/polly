53f5975e30d5c2d8e95844c4bbc79e87372f9f03
AdamSundqvist <adamsundqvist22@gmail.com>
2021-11-25 14:27:08 +0100

    Tar bort head

--- src/main.js
+++ src/main.js
@@ -1,26 +1,11 @@
 /* Lucas Wallin är en cool kille */
 /* Marcus är också cool */
 /*Elsa är coolast*/
-<<<<<<< HEAD
-=======
 /* Marcus är också cool! */
-<<<<<<< HEAD
-
-=======
 /*Elsa är coolast och adam med*/
-<<<<<<< HEAD
 /* Johanna är också med u guys*/
 /*Elsa testar igen*/
 
-
-=======
-<<<<<<< HEAD
->>>>>>> e0df554cbf7ed4cc5fb6b991ac0b6760f17e553c
->>>>>>> f57b579a61ecba5f0c00aec4febe92e1ef35b03f
-=======
->>>>>>> 355faa9c8be53b995e264729f458bbe6b23d9b89
->>>>>>> 3419eca36217461452e49a1564b64db792a29b7a
->>>>>>> a1e4075490e0cfc1b25f7c44232a360c25ffb5c1
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
