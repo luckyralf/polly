220632bec2d2846dfeb4ad1237c6317a4391d6f4
AdamSundqvist <adamsundqvist22@gmail.com>
2021-11-25 15:44:18 +0100

    skapa poll knapp

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,7 +3,7 @@
     Poll link: 
     <input type="text" v-model="pollId">
     <button v-on:click="createPoll">
-      Create poll
+      {{uiLabels.createPoll}}
     </button>
     <div>
       {{uiLabels.question}}:
