e0df554cbf7ed4cc5fb6b991ac0b6760f17e553c
AdamSundqvist <adamsundqvist22@gmail.com>
2021-11-25 13:50:18 +0100

    Adam e cool

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 /* Lucas Wallin är en cool kille */
-<<<<<<< HEAD
 /* Marcus är också cool */
-/*Elsa är coolast*/
-=======
-/* Marcus är också cool! */
->>>>>>> 523c218dee82c01dae2e56c1dcb7006201417c91
+/*Elsa är coolast och adam med*/
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
