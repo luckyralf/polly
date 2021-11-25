355faa9c8be53b995e264729f458bbe6b23d9b89
MacStellar <mar-98@hotmail.com>
2021-11-25 14:00:15 +0100

    Marcus tog bort alla <<<head grejer

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 /* Lucas Wallin är en cool kille */
 /* Marcus är också cool */
-<<<<<<< HEAD
 /*Elsa är coolast*/
 /* Marcus är också cool! */
-
-=======
 /*Elsa är coolast och adam med*/
->>>>>>> e0df554cbf7ed4cc5fb6b991ac0b6760f17e553c
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,41 +1,43 @@
 <template>
   <div id="nav">
-    <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
-    <router-link v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
+    <button v-on:click="switchLanguage">{{ uiLabels.changeLanguage }}</button>
+    <router-link v-bind:to="'/create/' + lang">{{
+      uiLabels.createPoll
+    }}</router-link>
     <label>
-      Write poll id: 
-      <input type="text" v-model="id">
+      Write poll id:
+      <input type="text" v-model="id" />
     </label>
-    <router-link v-bind:to="'/poll/'+id" tag="button">{{uiLabels.participatePoll}}</router-link>
+    <router-link v-bind:to="'/poll/' + id" tag="button">{{
+      uiLabels.participatePoll
+    }}</router-link>
   </div>
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
 const socket = io();
 
 export default {
-  name: 'Start',
+  name: "Start",
   data: function () {
     return {
       uiLabels: {},
       id: "",
-      lang: "en"
-    }
+      lang: "en",
+    };
   },
   created: function () {
     socket.on("init", (labels) => {
-      this.uiLabels = labels
-    })
+      this.uiLabels = labels;
+    });
   },
   methods: {
-    switchLanguage: function() {
-      if (this.lang === "en")
-        this.lang = "sv"
-      else
-        this.lang = "en"
-      socket.emit("switchLanguage", this.lang)
-    }
-  }
-}
+    switchLanguage: function () {
+      if (this.lang === "en") this.lang = "sv";
+      else this.lang = "en";
+      socket.emit("switchLanguage", this.lang);
+    },
+  },
+};
 </script>
