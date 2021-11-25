5ff3c4a1db84477ab364c3a0265a7b677ea4d775
MacStellar <mar-98@hotmail.com>
2021-11-25 13:24:57 +0100

    Marcus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* Lucas Wallin är en cool kille */
+/* Marcus är också cool */
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
