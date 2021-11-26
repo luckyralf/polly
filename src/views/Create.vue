42ac40b558fbea500995f0217ba1080bd05dcaaf
AdamSundqvist <adamsundqvist22@gmail.com>
2021-11-26 12:50:11 +0100

    löste språkjoxet till slut mvh adam

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Poll link:
+    {{uiLabels.pollLink}}
     <input type="text" v-model="pollId">
     <button v-on:click="createPoll">
       {{uiLabels.createPoll}}
@@ -41,7 +41,7 @@ export default {
       lang: "",
       pollId: "",
       question: "",
-      pollLink: "",
+      // pollLink: "",
       answers: ["", ""],
       questionNumber: 0,
       data: {},
