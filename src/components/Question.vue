677892590617e9c77d1995ac7db5f7f6de7aa003
AdamSundqvist <adamsundqvist22@gmail.com>
2021-11-28 22:21:40 +0100

    fixade färg vid klick

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,6 +1,6 @@
 <template>
 <p>{{question.q}}</p>
-<button v-for="a in question.a" v-on:click="answer(a);" v-bind:key="a" class="isClicked">
+<button v-for="(a, index) in question.a" v-on:click="answer(a); changeColor(index);" v-bind:key="a" class="isClicked">
   {{ a }}
 </button>
 </template>
@@ -14,13 +14,18 @@ export default {
     answer: function (answer) {
       this.$emit("answer", answer);
     },
-    // changeColor: function(a) {
-    //   a.backgroundColor = "#C73EE1";
-    // } FUNKAR EJ ÄN
+    changeColor: function(i) {
+      let answerButton = document.getElementsByClassName("isClicked")[i];
+      answerButton.classList.toggle("isClickd");
+    }   
   }
 }
 </script>
 <style>
+.isClickd {
+  background-color: #C73EE1;
+}
+
 .isClicked:hover {
   background-color:#D794E3;
 }
