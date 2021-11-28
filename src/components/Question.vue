9e9cb3ba3d6c8e0de48985ee449d9624b8f1a203
AdamSundqvist <adamsundqvist22@gmail.com>
2021-11-28 17:46:51 +0100

    liten färgändring på svarsalternativ, ej komplett

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,20 +1,30 @@
 <template>
 <p>{{question.q}}</p>
-<button v-for="a in question.a" v-on:click="answer(a)" v-bind:key="a" id="answerButton">
+<button v-for="a in question.a" v-on:click="answer(a);" v-bind:key="a" class="isClicked">
   {{ a }}
 </button>
 </template>
 <script>
 export default {
-  name: 'Bars',
+  name: 'Question',
   props: {
-    question: Object
+    question: Object,
   },
   methods: {
     answer: function (answer) {
       this.$emit("answer", answer);
-      document.getElementById("answerButton").style.backgroundColor = "#C73EE1";
-    } 
+    },
+    // changeColor: function(a) {
+    //   a.backgroundColor = "#C73EE1";
+    // } FUNKAR EJ ÄN
   }
 }
 </script>
+<style>
+.isClicked:hover {
+  background-color:#D794E3;
+}
+.isClicked:active {
+  background-color: #C73EE1;
+}
+</style>
