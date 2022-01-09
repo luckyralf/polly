8d15b83371109f576395c3695985b4d7855e3036
AdamSundqvist <adamsundqvist22@gmail.com>
2022-01-09 13:06:12 +0100

    showanswer borta

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -66,7 +66,6 @@ export default {
       questionNumber: 1,
       selectedAnswer: null, //index som anger vilket alternativ som är valt
       submittedAnswer: null, //index som anger vilket svarsalternativ som har skickats
-      showAnswer: false, //boolean som anger om ett svar har angetts för att visa "selected answer is:"
       answerSubmitted: false, //boolean som anger om man har skickat ett svar
       lastQuestion: true,
       quizFinished: false,
@@ -100,18 +99,14 @@ export default {
   },
   methods: {
     submitAnswer: function () {
-      //skriv ut vad det valda alternativet är
-      this.showAnswer = true;
       this.submittedAnswer = this.question.a[this.selectedAnswer];
       console.log(this.submittedAnswer);
       this.answerSubmitted = true;
-      //this.questionNumber = this.questionNumber + 1;
     },
     answer: function () {
       if (this.answerSubmitted == true && this.submittedAnswer != null) {
         this.$emit("answer", this.submittedAnswer);
         this.selectedAnswer = null;
-        this.showAnswer = false;
         this.answerSubmitted = false;
         this.submittedAnswer = null;
       }
